85b5917f8e3e189a0a4ad2c7b8088919ff0205a8
Matias Garcete <matim.garcete@gmail.com>
2024-11-28 00:51:39 -0300

    configuracion de sprint boot

---
+++ backend/src/test/java/com/sgc/CineApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
